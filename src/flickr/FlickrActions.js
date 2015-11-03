72ea093949a0e377516de5b37728cbc57df03889
Nick Nance <nance.nick@gmail.com>
2015-11-02 21:29:36 -0800

    small improvements to ajax request

--- src/flickr/FlickrActions.js
+++ src/flickr/FlickrActions.js
@@ -3,6 +3,11 @@ import Backbone from 'backbone';
 import request from 'superagent';
 import jsonp from 'superagent-jsonp';
 
+let flickrAPIAccess = {
+  api_key: '616f7adb3037eaad399f7bdb9c36207c',
+  api_secret: 'c1538d30934881e1'
+}
+
 // hack for handling jsonp response from flicker
 window.jsonFlickrFeed = function(results) {
   if (results && results.items) {
@@ -20,8 +25,15 @@ module.exports = {
       query: query
     });
     request
-      .get('https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=' + query)
+      .get('https://api.flickr.com/services/feeds/photos_public.gne')
+      // .query(flickrAPIAccess)
+      .query({format: 'json'})
+      .query({tags: 'query'})
       .use(jsonp)
-      .end();
+      .end(function(err, res){
+        if (!err) {
+          console.dir(res.body);
+        }
+      });
   }
 };
